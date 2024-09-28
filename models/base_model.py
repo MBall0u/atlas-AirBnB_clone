53b1d0e62a653275f9e04197141a743eb501edff
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-09-28 13:02:19 -0500

    M - 1:01 PM - added docstring template into code so I can fill them at work

--- models/base_model.py
+++ models/base_model.py
@@ -1,20 +1,61 @@
 #!/usr/bin/python3
+"""
+
+
+
+Returns:
+    _type_: _description_
+"""
+
+
 import uuid
 from datetime import datetime
 
 class BaseModel:
+    """
+
+
+
+    """
+
     def __init__(self):
+        """
+
+
+
+        """
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
+        """
+
+
+
+        """
+
         print "[BaseModel] ({}) {}".format(self.id, self.__dict__)
 
     def save(self):
+        """
+
+
+
+        """
+
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """
+
+        
+
+        Returns:
+            _type_: _description_
+        """
+
         my_dict = self.__dict__.copy()
         my_dict['created_at'] = self.created_at.isoformat()
         my_dict['updated_at'] = self.updated_at.isoformat()
