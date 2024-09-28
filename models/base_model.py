e22c5b38ed54d85e8c89b6367f17e16387972db6
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-09-28 15:22:38 +0000

    M - 10:21 AM - made the base_model file and added the class that is intialized with datetime and uuid, then made the public methods save and to_dict, have not added docstrings yet

---
+++ models/base_model.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        print "[BaseModel] ({}) {}".format(self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        my_dict = self.__dict__.copy()
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        my_dict['__class__'] = self.__class__.__name__
+        return my_dict
